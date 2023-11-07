5fb2e9fbb72ace53b4cecd6ef776720b2e12db37
Kruthibharath <kruthi.bharath@gmail.com>
2023-11-07 23:35:34 +0000

    feat: converted fetch() to async/await

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,22 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+/*const fetchData = (apiEndPoint) => {
+  fetch(apiEndPoint);
+.then((response) => response.json())
+.then((json) => console.log(json))  
+.catch((error) => console.log(error));
+};*/
+
+const fetchData = async (apiEndPoint) => {
+  try {
+    const response = await fetch(apiEndPoint);
+    console.log(`Yay! Promise resolved with response:${response}`);
+    const json = await response.json();
+    console.log(`Yay! Promise resolved with json:${json}`);
+  } catch (error) {
+    console.log(`Boo. Promise rejected with response:${error}`);
+  }
 };
 
 fetchData(jsonTypicode);
